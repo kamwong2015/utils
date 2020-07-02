944c0813f58dcb6be6e0b45baf74f0e24544550c
kamwong2015 <kamwong@umich.edu>
2020-07-02 08:52:45 -0400

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -25,9 +25,7 @@ class Corpus(object):
 
     def get_data(self, path, batch_size=20):
         # Add words to the dictionary
-#         infile = open(path, 'r')
-        infile = urllib.request.urlopen(path) 
-        with infile as f:
+        with open(path, 'r') as f:
             tokens = 0
             for line in f:
                 words = line.split() + ['<eos>']
